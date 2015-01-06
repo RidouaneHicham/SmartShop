710bfa89aadb200d2f0b80ec622e32e34f970b94
RidouaneHicham <hicham.ridouane@gmail.com>
2015-01-06 11:25:18 +0100

    second commit with updated packages

---
+++ app/src/main/java/com/hichamridouane/smartshop/model/ParseApplication.java
@@ -0,0 +1,31 @@
+package com.hichamridouane.smartshop.model;
+
+/**
+ * Created by Hicham Ridouane on 05/07/2014.
+ */
+import com.hichamridouane.smartshop.R;
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    static final String TAG = "MyApp";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "eUxEGbrPnNmn0UMdp070lopZHlHuyXtr0llNJmZF", "gAaIiqunLDMcysQiIPAIZCB0FJAsImYk1JGTVq34") ;
+        ParseFacebookUtils.initialize(getString(R.string.APP_ID));
+        PushService.setDefaultPushCallback(this,MainActivity.class);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+
+    }
+
+}
